361e09b5974246cbfeb962b42347bebaa3dfcfc1
PhantomTrainer2 <gallobreno@gmail.com>
2024-09-30 12:42:52 -0300

    Formatacao

--- lab5/lab5Q1a.c
+++ lab5/lab5Q1a.c
@@ -8,34 +8,41 @@ Breno Pinheiro Gallo de Sá 2110183*/
 
 int vetor[TAM_VET];
 
-void* roda(void* ini){
-  int inicio = *((int*)ini);
+void *roda(void *ini)
+{
+  int inicio = *((int *)ini);
   int fim = inicio + 1000;
   int soma = 0;
-  for (int i = inicio; i < fim; i++){
-    vetor[i]*=2;
+  for (int i = inicio; i < fim; i++)
+  {
+    vetor[i] *= 2;
     soma += vetor[i];
   }
   pthread_exit(NULL);
 }
 
-int main(){
+int main()
+{
   pthread_t threads[NUM_THREADS];
   clock_t tempoI = clock();
   int t;
   int inicio[NUM_THREADS];
-  int soma=0;
-  for (int i = 0; i < TAM_VET; i++){
+  int soma = 0;
+  for (int i = 0; i < TAM_VET; i++)
+  {
     vetor[i] = 5;
   }
-  for(t=0;t < NUM_THREADS;t++){
+  for (t = 0; t < NUM_THREADS; t++)
+  {
     inicio[t] = t * 1000;
     pthread_create(&threads[t], NULL, roda, &inicio[t]);
   }
-  for(t=0; t < NUM_THREADS; t++){
-    pthread_join(threads[t],NULL);
+  for (t = 0; t < NUM_THREADS; t++)
+  {
+    pthread_join(threads[t], NULL);
   }
-  for (int j = 0; j < TAM_VET; j++){
+  for (int j = 0; j < TAM_VET; j++)
+  {
     soma += vetor[j];
   }
   clock_t tempoF = clock();
--- lab5/lab5Q2b.c
+++ lab5/lab5Q2b.c
@@ -6,26 +6,34 @@ Breno Pinheiro Gallo de Sá 2110183*/
 #define NUM_THREADS 100
 
 int vetor[TAM_VET];
-void *roda(void *arg) {
-  for (int i = 0; i < TAM_VET; i++) {
+void *roda(void *arg)
+{
+  for (int i = 0; i < TAM_VET; i++)
+  {
     vetor[i] = (vetor[i] * 2) + 2;
   }
   return NULL;
 }
 
-int main() {
+int main()
+{
   pthread_t threads[NUM_THREADS];
-  for (int i = 0; i < TAM_VET; i++) {
+  for (int i = 0; i < TAM_VET; i++)
+  {
     vetor[i] = 5;
   }
-  for (int i = 0; i < NUM_THREADS; i++) {
+  for (int i = 0; i < NUM_THREADS; i++)
+  {
     pthread_create(&threads[i], NULL, roda, NULL);
   }
-  for (int i = 0; i < NUM_THREADS; i++) {
+  for (int i = 0; i < NUM_THREADS; i++)
+  {
     pthread_join(threads[i], NULL);
   }
-  for (int i = 1; i < TAM_VET; i++) {
-    if (vetor[i] != ((vetor[i-1]*2) + 2)) {
+  for (int i = 1; i < TAM_VET; i++)
+  {
+    if (vetor[i] != ((vetor[i - 1] * 2) + 2))
+    {
       printf("Há diferenças entre as posições!\n");
       return 0;
     }
